1dcd83df317b79c71efb418d9aa77a3ccdae36a2
Emon <schrodingersrats@gmail.com>
2023-07-03 22:41:39 +0600

    Create Test.cpp

---
+++ Sublime_Setup/Test.cpp
@@ -0,0 +1,18 @@
+#include<bits/stdc++.h>
+using namespace std;
+typedef long long ll;
+
+int main() {
+    ios::sync_with_stdio(false);
+    cin.tie(0);
+    int tt;
+    cin >> tt;
+    while(tt--) {
+        int n;
+        cin >> n;
+        vector<int> ele(n);
+        for(int i = 0; i < n; i++) {
+            cin >> ele[i];
+        }
+    }
+}
