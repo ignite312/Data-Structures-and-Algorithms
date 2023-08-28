1c4093a523e85ae4dedd0e5425fc2a5532bc2b96
Emon <schrodingersrats@gmail.com>
2023-08-28 23:31:05 +0600

    Update BellmanFord.cpp

--- Graph/BellmanFord.cpp
+++ Graph/BellmanFord.cpp
@@ -1,40 +1,42 @@
 #include<bits/stdc++.h>
 using namespace std;
-typedef long long ll;
+
+#define ll long long
 const ll INF = LLONG_MAX;
 vector<tuple<int, int, int>> edges;
-int n, m, st = 1;
 
-void BellmanFord() {
-    vector<ll> dist(n+1, INF);
-    dist[st] = 0;
-    for (;;) {
-        bool any = false;
-        for (auto[u, v, cost] : edges)
-            if (dist[u] < INF)
-                if (dist[v] > dist[u] + cost) {
-                    dist[v] = dist[u] + cost;
+void BellmanFord(int st, int n) {
+    vector<ll> dist(n + 1, INF);
+    dist[st] = 0;  
+    for (int i = 0; i < n - 1; i++) {
+        bool any = false;  
+        for (auto [u, v, wt] : edges) {
+            if (dist[u] < INF) {
+                if (dist[u] + wt < dist[v]) {
+                    dist[v] = dist[u] + wt;
                     any = true;
                 }
-        if (!any)
-            break;
+            }
+        }
+        if(!any)break;
     }
-    for(int i = 1; i <= n; i++) {cout << dist[i] << " ";}
+    for(int i = 1; i <= n; i++) {cout << dist[i] << " ";}   
 }
 int main() {
-  ios::sync_with_stdio(false);
-  cin.tie(0);
-  int tt;
-  tt = 1;
-  while(tt--) {
-    cin >> n >> m;
-    for(int i = 1; i <= m; i++) {
-        int u, v;
-        ll cost;
-        cin >> u >> v >> cost;
-        edges.emplace_back(u, v, cost);
+    ios::sync_with_stdio(false);
+    cin.tie(0);    
+    int tt;
+    tt = 1;    
+    while (tt--) {
+        int n, m;
+        cin >> n >> m;       
+        for (int i = 0; i < m; i++) {
+            int u, v, wt;
+            cin >> u >> v >> wt;
+            edges.emplace_back(u, v, wt);
+        }
+        BellmanFord(1, n);
     }
-    BellmanFord();
-  }
+    return 0;
 }
 // https://cses.fi/problemset/task/1671
