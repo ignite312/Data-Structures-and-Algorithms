2e76d94b02a0a2ffd862515b80a58f0c9da01e5f
Emon <schrodingersrats@gmail.com>
2023-12-30 01:07:28 +0600

    Update RangeUpdateSum(Lazy).cpp

--- SegmentTree/RangeUpdateSum(Lazy).cpp
+++ SegmentTree/RangeUpdateSum(Lazy).cpp
@@ -2,73 +2,6 @@
 using namespace std;
 #define ll long long
  
-struct Segtree {
-    int n;
-    vector<ll> tree, lazy;
- 
-    ll merge(ll x, ll y) {
-        return x + y;
-    }
-    void push(int node, int l, int r) {
-        tree[node]+=(r-l+1)*lazy[node];
-        int a = node*2+1, b = node*2+2;
-        lazy[a]+=lazy[node], lazy[b]+=lazy[node];
-        lazy[node] = 0;
-    }
-    void build(vector<ll> &a, int node, int l, int r) {
-        if(l == r) {
-            tree[node] = a[l];
-            return;
-        }
-        int mid = l + (r - l)/2;
-        build(a, node*2+1, l, mid);
-        build(a, node*2+2, mid+1, r);
-        tree[node] = merge(tree[node*2+1], tree[node*2+2]);
-    }
-    void build(vector<ll> &a) {
-        build(a, 0, 0, n-1);
-    }
-    void update(int i, int j, ll value, int node, int l, int r) {
-        if(l > j || r < i)return;
-        if(l >= i && r <= j) {
-            lazy[node]+=value;
-            return;
-        }
-        if(lazy[node])push(node, l, r);
-        int mid = l + (r-l)/2;
-        update(i, j, value, node*2+1, l, mid);
-        update(i, j, value, node*2+2, mid+1, r);
-        tree[node] = merge(tree[node*2+1], tree[node*2+2]);
-    }
-    void update(int i, int j, ll value) {
-        update(i, j, value, 0, 0, n-1);
-    }
-    ll query(int i, int j, int node, int l, int r) {
-        if(l > j || r < i)
-            return 0;
-        if(l >= i && r <= j)
-            return tree[node] + (r-l+1)*lazy[node];
- 
-        if(lazy[node]) push(node, l, r);
-        int mid = l + (r - l)/2;
-        return merge(query(i, j, node*2+1, l, mid), query(i, j, node*2+2, mid+1, r));
-    }
-    ll query(int i, int j) {
-        return query(i, j, 0, 0, n-1);
-    }
-    int sz(int n) {
-        int size = 1;
-        while(size < n) size = size << 1;
-        return 2*size-1;
-    }
-    void init(vector<ll> &a) {
-        n = a.size();
-        int _sz = sz(n);
-        tree.resize(_sz);
-        lazy.assign(_sz, 0);
-        build(a, 0, 0, n-1);
-    }
-} st;
 /* Static Update
 struct Segtree {
     int n;
@@ -122,6 +55,75 @@ struct Segtree {
     }
 } st;
 */
+struct Segtree {
+    int n;
+    vector<ll> tree, lazy;
+ 
+    ll merge(ll x, ll y) {
+        return x + y;
+    }
+    void push(int node, int l, int r) {
+        int a = node*2+1, b = node*2+2;
+        int mid = l + (r-l)/2;
+        tree[a]+=(mid-l+1)*lazy[node], tree[b]+=(r-(mid+1)+1)*lazy[node];
+        lazy[a]+=lazy[node], lazy[b]+=lazy[node];
+        lazy[node] = 0;
+    }
+    void build(vector<ll> &a, int node, int l, int r) {
+        if(l == r) {
+            tree[node] = a[l];
+            return;
+        }
+        int mid = l + (r - l)/2;
+        build(a, node*2+1, l, mid);
+        build(a, node*2+2, mid+1, r);
+        tree[node] = merge(tree[node*2+1], tree[node*2+2]);
+    }
+    void build(vector<ll> &a) {
+        build(a, 0, 0, n-1);
+    }
+    void update(int i, int j, ll value, int node, int l, int r) {
+        if(l > j || r < i)return;
+        if(l >= i && r <= j) {
+            lazy[node]+=value;
+            tree[node]+=(r-l+1)*value;
+            return;
+        }
+        if(lazy[node])push(node, l, r);
+        int mid = l + (r-l)/2;
+        update(i, j, value, node*2+1, l, mid);
+        update(i, j, value, node*2+2, mid+1, r);
+        tree[node] = merge(tree[node*2+1], tree[node*2+2]);
+    }
+    void update(int i, int j, ll value) {
+        update(i, j, value, 0, 0, n-1);
+    }
+    ll query(int i, int j, int node, int l, int r) {
+        if(l > j || r < i)
+            return 0;
+        if(l >= i && r <= j)
+            return tree[node];
+ 
+        if(lazy[node]) push(node, l, r);
+        int mid = l + (r - l)/2;
+        return merge(query(i, j, node*2+1, l, mid), query(i, j, node*2+2, mid+1, r));
+    }
+    ll query(int i, int j) {
+        return query(i, j, 0, 0, n-1);
+    }
+    int sz(int n) {
+        int size = 1;
+        while(size < n) size = size << 1;
+        return 2*size-1;
+    }
+    void init(vector<ll> &a) {
+        n = a.size();
+        int _sz = sz(n);
+        tree.resize(_sz);
+        lazy.assign(_sz, 0);
+        build(a, 0, 0, n-1);
+    }
+} st;
 int main() {
     ios::sync_with_stdio(false);
     cin.tie(0);
@@ -150,6 +152,7 @@ int main() {
     }
     return 0;
 }
+//*** https://cses.fi/problemset/task/1651/
 // https://cses.fi/problemset/task/1648
-// https://cses.fi/problemset/task/1651/
-// https://codeforces.com/contest/1872/problem/E
+// https://codeforces.com/contest/1872/submission/222996695
+// https://codeforces.com/contest/1915/submission/239536708
