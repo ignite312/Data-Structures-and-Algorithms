24e51186d8dc09479802c6f5f45f9ba6ca4450b0
ignite312 <contact.emonkhan@gmail.com>
2024-08-15 04:54:16 +0600

    Updated Template

--- Data Structure/SegmentUpdateIn2DPrefixSum.cpp
+++ Data Structure/SegmentUpdateIn2DPrefixSum.cpp
@@ -4,24 +4,23 @@ using namespace std;
 #define ll long long
 int n, m, k;
 
-void apply_increment(vector<vector<ll>>& diff, int x1, int y1, int x2, int y2, int val) {
-    diff[x1][y1] += val;
-    if (x2 + 1 < n) diff[x2 + 1][y1] -= val;
-    if (y2 + 1 < m) diff[x1][y2 + 1] -= val;
-    if (x2 + 1 < n && y2 + 1 < m) diff[x2 + 1][y2 + 1] += val;
+void update(vector<vector<ll>>& grid, int x1, int y1, int x2, int y2, int val) {
+    grid[x1][y1] += val;
+    if (x2 + 1 < n) grid[x2 + 1][y1] -= val;
+    if (y2 + 1 < m) grid[x1][y2 + 1] -= val;
+    if (x2 + 1 < n && y2 + 1 < m) grid[x2 + 1][y2 + 1] += val;
 }
-
-vector<vector<ll>> calculate(vector<vector<ll>> &diff) {
-    vector<vector<ll>> grid(n, vector<ll>(m, 0));
+vector<vector<ll>> calculate(vector<vector<ll>> &grid) {
+    vector<vector<ll>> ans(n, vector<ll>(m, 0));
     for (int i = 0; i < n; ++i) {
         for (int j = 0; j < m; ++j) {
-            grid[i][j] = diff[i][j];
-            if (i > 0) grid[i][j] += grid[i - 1][j];
-            if (j > 0) grid[i][j] += grid[i][j - 1];
-            if (i > 0 && j > 0) grid[i][j] -= grid[i - 1][j - 1];
+            ans[i][j] = grid[i][j];
+            if(i > 0) ans[i][j] += ans[i - 1][j];
+            if(j > 0) ans[i][j] += ans[i][j - 1];
+            if(i > 0 && j > 0) ans[i][j] -= ans[i - 1][j - 1];
         }
     }
-    return grid;
+    return ans;
 }
 int main() {
     ios::sync_with_stdio(false);
@@ -34,7 +33,7 @@ int main() {
         vector<vector<ll>> grid(n, vector<ll>(m, 0));
         for(int i = 0; i < n-k+1; i++) {
             for(int j = 0; j < m-k+1; j++) {
-                apply_increment(grid, i, j, i+k-1, j+k-1, 1);
+                update(grid, i, j, i+k-1, j+k-1, 1);
             }
         }
         int w;
@@ -44,19 +43,19 @@ int main() {
             cin >> wt[i];
         }
         grid = calculate(grid);
-        vector<ll> ans;
+        vector<ll> cnt;
         for (int i = 0; i < n; ++i) {
             for (int j = 0; j < m; ++j) {
-                ans.push_back(grid[i][j]);
+                cnt.push_back(grid[i][j]);
             }
         }
-        sort(ans.rbegin(), ans.rend());
+        sort(cnt.rbegin(), cnt.rend());
         sort(wt.rbegin(), wt.rend());
-        ll final = 0;
+        ll ans = 0;
         for(int i = 0; i < w; i++) {
-            final += wt[i] * ans[i];
+            ans += wt[i] * cnt[i];
         }
-        cout << final << "\n";
+        cout << ans << "\n";
     }
     return 0;
 }
