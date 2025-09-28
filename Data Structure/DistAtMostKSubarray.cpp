d2712da856ff77852b8d327723c4308e061532fa
ignite312 <contact.emonkhan@gmail.com>
2025-09-29 02:33:44 +0600

    .

---
+++ Data Structure/DistAtMostKSubarray.cpp
@@ -0,0 +1,42 @@
+// https://codeforces.com/contest/2149/problem/E
+#include<bits/stdc++.h>
+using namespace std;
+#define ll long long
+
+ll distAtMostK(vector<int> &v, int k, int mxlength) {
+    int n = v.size();
+    int dist = 0;
+    int l = 0;
+    ll ans = 0;
+    map<int, int> mp;
+    for(int r = 0; r < n; r++) {
+        if(++mp[v[r]] == 1) {
+            dist++;
+        }
+        while(dist > k || (r-l+1) > mxlength) {
+            if(--mp[v[l]] == 0)dist--;
+            l++;
+        }
+        ans+=(r-l+1)*1LL;
+    }
+    return ans;
+}
+
+int main() {
+    ios::sync_with_stdio(false);
+    cin.tie(0);
+    int tt;
+    tt = 1;
+    cin >> tt;
+    while(tt--) {
+        int n, k, l, r;
+        cin >> n >> k >> l >> r;
+        vector<int> v(n);
+        for(int i = 0; i < n; i++) {
+            cin >> v[i];
+        }
+        cout << (distAtMostK(v, k, r) - distAtMostK(v, k, l-1)) - 
+         (distAtMostK(v, k-1, r) - distAtMostK(v, k-1, l-1)) << "\n";
+    }
+    return 0;
+}
